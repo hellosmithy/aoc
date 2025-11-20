78db3cc3f2b4298738ca8586ba2ebe1812fd403b
Ben Smith <ben@hellosmithy.com>
2025-11-20 22:50:39 +0000

    2024 day 1 pt 2

--- 2024/src/day_1.py
+++ 2024/src/day_1.py
@@ -1,7 +1,9 @@
+from collections import Counter
 from pathlib import Path
 
 
 def load_data(data_path):
+    """Load data from the given path."""
     l_list = []
     r_list = []
     with open(data_path, newline="", encoding="utf-8") as f:
@@ -12,14 +14,35 @@ def load_data(data_path):
     return l_list, r_list
 
 
+def count_occurrences(seq):
+    """Return a dict mapping value -> count for seq. See: https://realpython.com/python-counter/"""
+    return dict(Counter(seq))
+
+
 def compute_total_distance(l_list, r_list):
+    """Compute the total distance between two lists."""
     return sum(abs(l - r) for l, r in zip(sorted(l_list), sorted(r_list)))
+
+
+def compute_total_similarity(l_list, r_list):
+    """Compute the total similarity between two lists."""
+    r_counts = count_occurrences(r_list)
+    total_similarity = 0
+    for l in l_list:
+        total_similarity += l * r_counts.get(l, 0)
+    return total_similarity
 
 
 def main():
     l, r = load_data(Path(__file__).parent / "day_1_data.txt")
-    result = compute_total_distance(l, r)
-    print(result)
+    total_distance = compute_total_distance(l, r)
+    total_similarity = compute_total_similarity(l, r)
+    print(
+        f"""
+        Total Distance: {total_distance}
+        Total Similarity: {total_similarity}
+        """
+    )
 
 
 if __name__ == "__main__":
--- 2024/tests/test_day_1.py
+++ 2024/tests/test_day_1.py
@@ -1,7 +1,13 @@
-from day_1 import compute_total_distance
+from day_1 import compute_total_distance, compute_total_similarity
 
 
 def test_compute_total_distance():
     left = [3, 4, 2, 1, 3, 3]
     right = [4, 3, 5, 3, 9, 3]
     assert compute_total_distance(left, right) == 11
+
+
+def test_compute_total_similarity():
+    left = [3, 4, 2, 1, 3, 3]
+    right = [4, 3, 5, 3, 9, 3]
+    assert compute_total_similarity(left, right) == 31
